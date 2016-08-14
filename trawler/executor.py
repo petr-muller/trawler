2fbf1b39198fe015e6f59942d32771b4a7c62ec2
Petr Muller <muller@redhat.com>
2016-08-14 22:50:27 +0200

    Add some type definitions for mypy

--- trawler/executor.py
+++ trawler/executor.py
@@ -6,25 +6,29 @@ Created on Jul 30, 2016
 import subprocess
 import shlex
 from pathlib import Path
+from typing import List, Any, IO
 
 class Executor(object):
     """
     This module implements actions over project repositories.
     """
 
-    def __init__(self, repo_path, compile_recipe, test_recipe, clean_recipe):
+    def __init__(self, repo_path: str, compile_recipe: List[str], test_recipe: List[str],
+                 clean_recipe: List[str]) -> None:
         self.repo_path = Path(repo_path)
         self.compile_recipe = compile_recipe
         self.test_recipe = test_recipe
         self.clean_recipe = clean_recipe
 
     @staticmethod
-    def _run_recipe(recipe, output=subprocess.DEVNULL, check=True):
+    def _run_recipe(recipe: List[str], output: IO[Any]=subprocess.DEVNULL, # type: ignore
+                    check: bool=True) -> None:
         for item in recipe:
             to_run = shlex.split(item)
-            subprocess.run(to_run, stdout=output, stderr=subprocess.STDOUT, check=check)
+            subprocess.run(to_run, stdout=output, stderr=subprocess.STDOUT,  # type: ignore
+                           check=check)
 
-    def compile(self, output_file_path):
+    def compile(self, output_file_path: str) -> None:
         """
         Execute a recipe to compile the software project.
         """
@@ -32,7 +36,7 @@ class Executor(object):
             with self.repo_path:
                 Executor._run_recipe(self.compile_recipe, output=output_file, check=True)
 
-    def test(self, output_file_path):
+    def test(self, output_file_path: str) -> None:
         """
         Execute a recipe to run a projects testsuite.
         """
@@ -40,7 +44,7 @@ class Executor(object):
             with self.repo_path:
                 Executor._run_recipe(self.test_recipe, output=output_file, check=False)
 
-    def clean(self):
+    def clean(self) -> None:
         """
         Execute a recipe to clean the projects build state.
         """
